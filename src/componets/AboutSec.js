0a77bf9a4f610b0e74772cac42e915c508ed78ca
Patrick Eagar <Patrick_Eagar@vfc.com>
2024-06-25 13:23:29 -0700

    add - to front end

--- src/componets/AboutSec.js
+++ src/componets/AboutSec.js
@@ -16,7 +16,7 @@ const AboutSec = () => {
                     <div className="info-box info-box--about">
                         <h3 class="info-title">Who am I?</h3>
                         <p class="info-box--text">
-                            I'm a <strong>Frontend Web Developer</strong> with a passion for building and managing the front-end of websites and web applications. My goal is to create user-friendly, visually appealing, and highly functional interfaces that contribute to the overall success of the product. I thrive on solving complex problems and bringing creative designs to life through clean, efficient code. Whether it's enhancing user experience or optimizing performance, I'm dedicated to delivering top-notch results. Check out some of my work in the <strong>Projects</strong> section to see the range and depth of my expertise.
+                            I'm a <strong>Front-end Web Developer</strong> with a passion for building and managing the front-end of websites and web applications. My goal is to create user-friendly, visually appealing, and highly functional interfaces that contribute to the overall success of the product. I thrive on solving complex problems and bringing creative designs to life through clean, efficient code. Whether it's enhancing user experience or optimizing performance, I'm dedicated to delivering top-notch results. Check out some of my work in the <strong>Projects</strong> section to see the range and depth of my expertise.
                         </p>
                         <p class="info-box--text">
                             If you have job opportunities where I can contribute, learn, and grow, I'd love to connect. Please don't hesitate to <a href="mailto:hi@pateagar.com"> get in touch.</a>
